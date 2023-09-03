7b5e5e631938b3ff70dde247b72a71b40fce6821
Talich12 <denis.tal@mail.ru>
2023-09-03 16:38:16 +1000

    refactor: Рефактор кода под новые задачи

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import cv2
 import numpy as np
-from blurgenerator import motion_blur_with_depth_map
 
 def depth_map_overlay(image_path, depth_map_path):
 
     # Загрузка изображений
     img = cv2.imread(image_path)
-    depth_img = cv2.imread(depth_map_path)
 
-    depth_img = cv2.resize(depth_img, (img.shape[1], img.shape[0]))
+    # Загрузите изображение в оттенках серого
+    depth_img = cv2.imread(depth_map_path , cv2.IMREAD_GRAYSCALE)
 
-    result = motion_blur_with_depth_map(
-    img,
-    depth_map=depth_img,
-    angle=30,
-    num_layers=10,
-    min_blur=1,
-    max_blur=50
-    )
+    # Преобразуйте изображение в цветовую карту
+    colored_depth_img = cv2.applyColorMap(depth_img, cv2.COLORMAP_JET)
 
+    # Сохраните преобразованное изображение
+    cv2.imwrite('colored_depth_map.png', colored_depth_img)
+    alpha = 0.5
 
-    cv2.imshow('Overlay', result)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imwrite('./output.png', result)
+    # Наложите изображения
+    overlay = cv2.addWeighted(img, alpha, colored_depth_img, 1 - alpha, 0)
+
+    #cv2.imshow('Overlay', overlay)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    cv2.imwrite('./output.png', overlay)
 
 depth_map_overlay('depth-test.jpg', 'depth-map-test.png')
